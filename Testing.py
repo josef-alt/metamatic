badc1c01764fedafe3fb272eff7ffc93132b5685
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-02-24 15:35:12 -0600

    restructured trie testing

--- Testing.py
+++ Testing.py
@@ -1,17 +1,37 @@
 from ClosestMatch import Trie
+import time
 
 test_trie = Trie()
-print('loading trie')
-with open('testing_insert') as input_file:
-	for entry in input_file:
-		print('inserting', entry, end='')
-		test_trie.insert(entry)
+
+print('LOADING TRIE')
+with open('testing_insert_large') as input_file:
+    start = time.time()
+    for entry in input_file:
+        test_trie.insert(entry.rstrip())
+    end = time.time()
+    print(f'loaded {test_trie.entries} words in {end - start:.4f}s')
 print('\n')
 
-print('executing queries')
-with open('testing_find_exact') as input_file:
-	for query in input_file:
-		if test_trie.find_exact(query):
-			print('found', query, end='')
-		else:
-			print('did not find', query, end='')
+print('EXECUTING QUERIES - find exact')
+with open('testing_find') as input_file:
+    for query in input_file:
+        query = query.rstrip()
+        start = time.time()
+        ans = test_trie.find_exact(query)
+        end = time.time()
+        print(f'\'{query}\' was{"" if ans else " NOT"} found in {end - start:.4f}s')
+print('\n')
+
+print('EXECUTING QUERIES - find nearest')
+with open('testing_find') as input_file:
+    for query in input_file:
+        query = query.rstrip()
+        print(f'looking for \'{ query }\'')
+        start = time.time()
+        ans = test_trie.find_nearest(query)
+        end = time.time()
+        if ans:
+            print(f'found {min(ans)} in {end - start:.4f}s')
+        else:
+            print(f'no matches found in {end - start:.4f}s')
+
